5e99284dd09c887c86aa830bd83b71c119e48b98
Fernando <fernando.madser@gmail.com>
2019-07-04 23:40:14 -0500

    Users get saved to the DB

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body;
@@ -25,7 +27,36 @@ router.post('/register', (req, res) => {
       password2
     })
   } else {
-    res.send('Registered')
+    User.findOne({ email: email })
+      .then( user => {
+        if (user) {
+          errors.push({ msg: 'Email is already registered' })
+          res.sendFile(path.join(__dirname, '../views/register.html'), {
+            errors,
+            name,
+            email,
+            password,
+            password2
+          })
+        } else {
+          const newUser = new User({
+            name,
+            email,
+            password
+          })
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user =>{
+                  res.redirect('/login');
+                })
+                .catch(err => console.log('Error when saving User: ', err));
+            })
+          })
+        }
+      });
   }
 })
 
